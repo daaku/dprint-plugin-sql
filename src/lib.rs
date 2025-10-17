1a88ff4983cc35189e2a759671e777ab12b29197
Naitik Shah <n@daaku.org>
2025-10-17 08:31:59 +0530

    less indirections

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ use dprint_core::configuration::RECOMMENDED_GLOBAL_CONFIGURATION;
 use dprint_core::configuration::get_unknown_property_diagnostics;
 use dprint_core::configuration::get_value;
 use dprint_core::configuration::resolve_new_line_kind;
-use dprint_core::configuration::{ConfigKeyMap, ConfigKeyValue, GlobalConfiguration};
+use dprint_core::configuration::{ConfigKeyMap, GlobalConfiguration};
 use dprint_core::plugins::CheckConfigUpdatesMessage;
 use dprint_core::plugins::ConfigChange;
 use dprint_core::plugins::FormatResult;
@@ -28,101 +28,18 @@ pub struct Configuration {
     pub lines_between_queries: u8,
 }
 
-#[derive(Default)]
-pub struct ConfigurationBuilder {
-    config: ConfigKeyMap,
-    global_config: Option<GlobalConfiguration>,
-}
-
-impl ConfigurationBuilder {
-    /// Constructs a new configuration builder.
-    pub fn new() -> ConfigurationBuilder {
-        Self::default()
-    }
-
-    /// Gets the final configuration that can be used to format a file.
-    pub fn build(&self) -> Configuration {
-        if let Some(global_config) = &self.global_config {
-            resolve_config(self.config.clone(), global_config).config
-        } else {
-            resolve_config(self.config.clone(), &Default::default()).config
-        }
-    }
-
-    /// Set the global configuration.
-    pub fn global_config(&mut self, global_config: GlobalConfiguration) -> &mut Self {
-        self.global_config = Some(global_config);
-        self
-    }
-
-    /// Whether to use tabs (true) or spaces (false).
-    ///
-    /// Default: `false`
-    pub fn use_tabs(&mut self, value: bool) -> &mut Self {
-        self.insert("useTabs", value.into())
-    }
-
-    /// The number of columns for an indent.
-    ///
-    /// Default: `4`
-    pub fn indent_width(&mut self, value: u8) -> &mut Self {
-        self.insert("indentWidth", (value as i32).into())
-    }
-
-    /// The kind of newline to use.
-    /// Default: `NewLineKind::LineFeed`
-    pub fn new_line_kind(&mut self, value: NewLineKind) -> &mut Self {
-        self.insert("newLineKind", value.to_string().into())
-    }
-
-    /// Use ALL CAPS for reserved words.
-    /// Default: `false`
-    pub fn uppercase(&mut self, value: bool) -> &mut Self {
-        self.insert("uppercase", value.into())
-    }
-
-    /// Number of line breaks between queries.
-    /// Default: `1`
-    pub fn lines_between_queries(&mut self, value: u8) -> &mut Self {
-        self.insert("linesBetweenQueries", (value as i32).into())
-    }
-
-    #[cfg(test)]
-    fn get_inner_config(&self) -> ConfigKeyMap {
-        self.config.clone()
-    }
-
-    fn insert(&mut self, name: &str, value: ConfigKeyValue) -> &mut Self {
-        self.config.insert(String::from(name), value);
-        self
+impl Default for Configuration {
+    fn default() -> Self {
+        SqlPluginHandler::new()
+            .resolve_config(Default::default(), &Default::default())
+            .config
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use dprint_core::configuration::{NewLineKind, resolve_global_config};
-
     use super::*;
-
-    #[test]
-    fn check_all_values_set() {
-        let mut config = ConfigurationBuilder::new();
-        config
-            .new_line_kind(NewLineKind::CarriageReturnLineFeed)
-            .use_tabs(true)
-            .indent_width(4)
-            .uppercase(true)
-            .lines_between_queries(2);
-
-        let inner_config = config.get_inner_config();
-        assert_eq!(inner_config.len(), 5);
-        let diagnostics = resolve_config(
-            inner_config,
-            &resolve_global_config(&mut Default::default()).config,
-        )
-        .diagnostics;
-        assert_eq!(diagnostics.len(), 0);
-    }
+    use dprint_core::configuration::{NewLineKind, resolve_global_config};
 
     #[test]
     fn handle_global_config() {
@@ -130,70 +47,19 @@ mod tests {
         global_config.insert(String::from("newLineKind"), "crlf".into());
         global_config.insert(String::from("useTabs"), true.into());
         let global_config = resolve_global_config(&mut global_config).config;
-        let mut config_builder = ConfigurationBuilder::new();
-        let config = config_builder.global_config(global_config).build();
-        assert_eq!(
-            config.new_line_kind == NewLineKind::CarriageReturnLineFeed,
-            true
-        );
+        let mut sph = SqlPluginHandler::new();
+        let config = sph
+            .resolve_config(Default::default(), &global_config)
+            .config;
+        assert_eq!(config.new_line_kind, NewLineKind::CarriageReturnLineFeed);
         assert_eq!(config.use_tabs, true);
     }
 
     #[test]
     fn use_defaults_when_global_not_set() {
-        let global_config = resolve_global_config(&mut Default::default()).config;
-        let mut config_builder = ConfigurationBuilder::new();
-        let config = config_builder.global_config(global_config).build();
+        let config = Configuration::default();
         assert_eq!(config.indent_width, 2);
-        assert_eq!(config.new_line_kind == NewLineKind::LineFeed, true);
-    }
-}
-
-pub fn resolve_config(
-    config: ConfigKeyMap,
-    global_config: &GlobalConfiguration,
-) -> PluginResolveConfigurationResult<Configuration> {
-    let mut diagnostics = Vec::new();
-    let mut config = config;
-
-    let resolved_config = Configuration {
-        use_tabs: get_value(
-            &mut config,
-            "useTabs",
-            global_config
-                .use_tabs
-                .unwrap_or(RECOMMENDED_GLOBAL_CONFIGURATION.use_tabs),
-            &mut diagnostics,
-        ),
-        indent_width: get_value(
-            &mut config,
-            "indentWidth",
-            global_config
-                .indent_width
-                .unwrap_or(RECOMMENDED_GLOBAL_CONFIGURATION.indent_width),
-            &mut diagnostics,
-        ),
-        new_line_kind: get_value(
-            &mut config,
-            "newLineKind",
-            global_config
-                .new_line_kind
-                .unwrap_or(RECOMMENDED_GLOBAL_CONFIGURATION.new_line_kind),
-            &mut diagnostics,
-        ),
-        uppercase: get_value(&mut config, "uppercase", false, &mut diagnostics),
-        lines_between_queries: get_value(&mut config, "linesBetweenQueries", 1, &mut diagnostics),
-    };
-
-    diagnostics.extend(get_unknown_property_diagnostics(config));
-
-    PluginResolveConfigurationResult {
-        config: resolved_config,
-        diagnostics,
-        file_matching: dprint_core::plugins::FileMatchingInfo {
-            file_extensions: vec!["sql".to_string()],
-            file_names: vec![],
-        },
+        assert_eq!(config.new_line_kind, NewLineKind::LineFeed);
     }
 }
 
@@ -238,7 +104,7 @@ pub fn format_text(text: &str, config: &Configuration) -> Result<Option<String>>
     }
 }
 
-struct SqlPluginHandler {}
+pub struct SqlPluginHandler {}
 
 impl SqlPluginHandler {
     #[allow(dead_code)]
@@ -253,7 +119,53 @@ impl SyncPluginHandler<Configuration> for SqlPluginHandler {
         config: ConfigKeyMap,
         global_config: &GlobalConfiguration,
     ) -> PluginResolveConfigurationResult<Configuration> {
-        resolve_config(config, global_config)
+        let mut diagnostics = Vec::new();
+        let mut config = config;
+
+        let resolved_config = Configuration {
+            use_tabs: get_value(
+                &mut config,
+                "useTabs",
+                global_config
+                    .use_tabs
+                    .unwrap_or(RECOMMENDED_GLOBAL_CONFIGURATION.use_tabs),
+                &mut diagnostics,
+            ),
+            indent_width: get_value(
+                &mut config,
+                "indentWidth",
+                global_config
+                    .indent_width
+                    .unwrap_or(RECOMMENDED_GLOBAL_CONFIGURATION.indent_width),
+                &mut diagnostics,
+            ),
+            new_line_kind: get_value(
+                &mut config,
+                "newLineKind",
+                global_config
+                    .new_line_kind
+                    .unwrap_or(RECOMMENDED_GLOBAL_CONFIGURATION.new_line_kind),
+                &mut diagnostics,
+            ),
+            uppercase: get_value(&mut config, "uppercase", false, &mut diagnostics),
+            lines_between_queries: get_value(
+                &mut config,
+                "linesBetweenQueries",
+                1,
+                &mut diagnostics,
+            ),
+        };
+
+        diagnostics.extend(get_unknown_property_diagnostics(config));
+
+        PluginResolveConfigurationResult {
+            config: resolved_config,
+            diagnostics,
+            file_matching: dprint_core::plugins::FileMatchingInfo {
+                file_extensions: vec!["sql".to_string()],
+                file_names: vec![],
+            },
+        }
     }
 
     fn check_config_updates(
--- tests/test.rs
+++ tests/test.rs
@@ -3,21 +3,20 @@ extern crate dprint_plugin_sql;
 
 use dprint_core::configuration::ConfigKeyMap;
 use dprint_core::configuration::resolve_global_config;
+use dprint_core::plugins::SyncPluginHandler;
 use dprint_development::ParseSpecOptions;
 use dprint_development::RunSpecsOptions;
 use dprint_development::ensure_no_diagnostics;
 use dprint_development::run_specs;
-use dprint_plugin_sql::ConfigurationBuilder;
-use dprint_plugin_sql::resolve_config;
+use dprint_plugin_sql::Configuration;
+use dprint_plugin_sql::SqlPluginHandler;
+use dprint_plugin_sql::format_text;
 use std::path::PathBuf;
 use std::sync::Arc;
-
-use dprint_plugin_sql::format_text;
 
 #[test]
 fn test_specs() {
     let global_config = resolve_global_config(&mut Default::default()).config;
-
     run_specs(
         &PathBuf::from("./tests/specs"),
         &ParseSpecOptions {
@@ -32,10 +31,10 @@ fn test_specs() {
             Arc::new(move |_file_path, file_text, spec_config| {
                 let spec_config: ConfigKeyMap =
                     serde_json::from_value(spec_config.clone().into()).unwrap();
-                let config_result = resolve_config(spec_config, &global_config);
+                let mut sph = SqlPluginHandler::new();
+                let config_result = sph.resolve_config(spec_config, &global_config);
                 ensure_no_diagnostics(&config_result.diagnostics);
-
-                format_text(&file_text, &config_result.config)
+                format_text(file_text, &config_result.config)
             })
         },
         Arc::new(move |_file_path, _file_text, _spec_config| {
@@ -46,7 +45,11 @@ fn test_specs() {
 
 #[test]
 fn should_handle_windows_newlines() {
-    let config = ConfigurationBuilder::new().build();
-    let file_text = format_text("SELECT * FROM  dbo.Test\r\n", &config).unwrap();
-    assert_eq!(file_text.unwrap(), "select\n  *\nfrom\n  dbo.Test\n");
+    let config = Configuration::default();
+    assert_eq!(
+        format_text("SELECT * FROM  dbo.Test\r\n", &config)
+            .unwrap()
+            .unwrap(),
+        "select\n  *\nfrom\n  dbo.Test\n",
+    );
 }
